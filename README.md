03b269c41a0e7aeffba406573b483f24096cae8e
Lance Johnson <lance.johnson550@gmail.com>
2024-08-04 16:49:16 -0700

    Completed Project
    Wrote and tested code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Course3FinalProject
  Final project for Coursera Course
+
+
+This script first loads the feature names removes the special characters ",-()". These feature names become the column names for the X data sets.
+
+The descriptive activity names are loaded in.
+
+The X, y, and subject data sets are loaded for both train and test. X data sets are reduced down to only the columns which contain "mean" or "std" in their names. The numerical labels in the y data sets are replaced with the descriptive activity names that were loaded in. The subjects and activity labels are added as new columns to the reduced X data sets. 
+
+The test and train data frames are appended to create a combined data frame. This is saved to "combined_data.csv", with headers.
+
+This data is grouped by subject and activity and summarized by taking the mean for each column. This summary data frame is saved as "summary_data.csv".
+
---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+library(dplyr)
+
+# Load the data sets
+#   Load features names
+features <- read.csv("UCI HAR Dataset/features.txt", sep = "", header = FALSE)
+feature_names <- features$V2
+#   Clean features names to remove special characters
+feature_names <- lapply(feature_names, function(x) gsub("-","_", x))
+feature_names <- lapply(feature_names, function(x) gsub(",","_", x))
+feature_names <- lapply(feature_names, function(x) gsub("\\(","", x))
+feature_names <- lapply(feature_names, function(x) gsub("\\)","", x))
+#   Load descriptive activity names
+activity_names <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
+
+#   Load the test sets
+x_test <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
+colnames(x_test) <- feature_names
+x_test_reduced <- x_test %>% select(contains("mean") | contains("std"))
+y_test <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
+y_test <- transform(y_test,V1 = activity_names[V1,2])
+subject_test <- read.csv("UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE)
+
+#   Load the train sets
+x_train <- read.csv("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
+colnames(x_train) <- feature_names
+x_train_reduced <- x_train %>% select(contains("mean") | contains("std"))
+y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
+y_train <- transform(y_train,V1 = activity_names[V1,2])
+subject_train <- read.csv("UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE)
+
+# Merge the data sets
+#   Add subject to each set
+x_train_reduced$subject <- subject_train$V1
+x_test_reduced$subject <- subject_test$V1
+#   Add labels to each set
+x_train_reduced$activity <- y_train$V1
+x_test_reduced$activity <- y_test$V1
+#   Append data sets
+final_data <- rbind(x_test_reduced,x_train_reduced)
+
+# Save the data set
+write.csv(final_data, "combined_data.csv", row.names = FALSE)
+
+# Create average for each subject and each activity
+summary_data <- final_data %>% group_by(subject, activity) %>%
+    summarize(across(ends_with("std") | ends_with("mean"), mean))
